96dcb29033bd632d1fa88b1ec25efa7c2bc3c095
PrikazchikovDA <vTIiB35VTDCt>
2020-11-17 19:39:59 +0500

    Это файл для генерации карточек с числами

---
+++ Генератор чисел.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+count = int(input('Введите число карточек: '))
+task = open("generated.txt", 'w')
+for k in range(1, 10):
+    for i in range(1, 10):
+        task.write(str(k))
+        task.write(" X ")
+        task.write(str(i))
+        task.write("\n")
+        task.write("\n")
+
+task.close()
